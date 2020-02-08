422e823ef2d216af5df063341813a86f87936d8b
ugaemi <u.gaemi@gmail.com>
2020-02-08 17:43:12 +0900

    #3.9 Getting the Weather

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "519ff010b5cc905ed29292b3e1c15dc0";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude,
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
