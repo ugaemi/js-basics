2e4da790d684f5ab7484f57edb59b0fb8bbbf22a
ugaemi <u.gaemi@gmail.com>
2020-02-08 18:07:11 +0900

    #3.10 Getting the Weather

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector('.js-weather');
 const API_KEY = "519ff010b5cc905ed29292b3e1c15dc0";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +26,7 @@ function handleGeoSuccess(position) {
         longitude,
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,7 +42,8 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
